c13fcbb5cdb17aef1f5ebfd0795a90201a10555b
md sahadat hossin <mdsahadathossin778@gmail.com>
2021-10-04 21:43:27 +0600

    add carousel

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row } from "react-bootstrap";
+import { Button, Carousel, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import Events from "../Events/Events";
 import Footer from "../Footer/Footer";
@@ -9,6 +9,10 @@ import Review from "../Review/Review";
 import SingleCourse from "../SinlgeCourse/SingleCourse";
 import "./Home.css";
 
+import sliderOne from "../../images/slider/4.jpg";
+import sliderTwo from "../../images/slider/5.jpg";
+import sliderThree from "../../images/slider/7.jpg";
+
 const Home = () => {
   // here import the custom hook function and use here
   const [courses] = useCourses();
@@ -17,19 +21,82 @@ const Home = () => {
     <div>
       {/* dispplay the menubar  */}
       <MenuBar></MenuBar>
-      {/* set the background image of home page */}
-      <div className="backgrund-img">
-        <div className="mx-auto w-50 text-center about-us">
-          <h1 className="main-heading">
-            Start learning from the
-            <span style={{ color: "#FF920F", paddingBottom: "10px" }}>
-              {" "}
-              World’s Best Institutions
-            </span>
-          </h1>
-          <Button variant="outline-warning">Learn More</Button>
-        </div>
+      {/* here add the carousel */}
+      <div className="carousel">
+        <Carousel fade>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 slider-img"
+              src={sliderOne}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <div className="mx-auto w-75 text-center  pb-5">
+                <h1 className="main-heading">
+                  Start learning from the
+                  <span
+                    style={{
+                      color: "#FF920F",
+                      paddingBottom: "10px",
+                      marginLeft: "10px",
+                    }}
+                  >
+                    World’s Best Institutions
+                  </span>
+                </h1>
+                <Button variant="outline-warning">Learn More</Button>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={sliderTwo} alt="Second slide" />
+
+            <Carousel.Caption>
+              <div className="mx-auto w-75 text-center  pb-5">
+                <h1 className="main-heading">
+                  Create Clean & Professional Multipurpose Landing Page
+                  <span
+                    style={{
+                      color: "#FF920F",
+                      paddingBottom: "10px",
+                      marginLeft: "10px",
+                    }}
+                  >
+                    Now ....
+                  </span>
+                </h1>
+                <Button variant="outline-warning">Learn More</Button>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={sliderThree}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <div className="mx-auto w-75 text-center pb-5">
+                <h1 className="main-heading">
+                  Learning how to code can change your life for
+                  <span
+                    style={{
+                      color: "#FF920F",
+                      paddingBottom: "10px",
+                      marginLeft: "10px",
+                    }}
+                  >
+                    Good.
+                  </span>
+                </h1>
+                <Button variant="outline-warning">Starts Now to Learn</Button>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
+      {/* courses sectin here */}
       <div className="all-Courses container py-5">
         <div className="Course-heading">
           <h1>Our Courses</h1>
